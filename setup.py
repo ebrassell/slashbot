8971884ad7cc4ed67474f17781d67a9cd423e672
Eric Brassell <ebrassell@indigoag.com>
2021-10-11 07:09:22 -0400

    Change to MIT License

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='slashbot',
     version='0.0.2',
-    license='BSD',
+    license='MIT',
     author="Eric Brassell",
     packages=find_packages('src'),
     package_dir={'': 'src'},
