45bffb9c55f47d1cef0e64a8dd6f7d1db5740cb2
Eric Brassell <ebrassell@indigoag.com>
2021-10-10 23:50:06 -0400

    Upload to pypi

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='slashbot',
+    version='0.0.2',
+    license='BSD',
+    author="Eric Brassell",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/ebrassell/slashbot',
+    keywords='slack bot slash slackbot lambda',
+    install_requires=['requests>=2.0.0'
+      ],
+    py_modules=['slashbot'],
+
+)
